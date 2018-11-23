4f55dafa4c60db8686984b90d0df4c8864ebf58d
sautry <43554772+sautry@users.noreply.github.com>
2018-11-23 13:08:45 -0600

    description
    descripot

---
+++ TeamProjectGUI/src/teamproject/School.java
@@ -0,0 +1,44 @@
+package teamproject;
+
+import java.util.ArrayList;
+
+public class School {
+	
+	ArrayList<student> students = new ArrayList<>();
+	ArrayList<admin> admins = new ArrayList<>();
+	
+	
+	public School(){
+		
+		
+		
+	}
+	
+    //make an array of admin
+    
+
+	//read from file to populate admin array
+	
+	
+	
+	//make an array of students
+
+	
+	
+	//read from file to populate student array
+
+	
+	
+	
+	
+	
+	
+	
+	//save to file
+	
+	
+	
+	
+
+
+}
